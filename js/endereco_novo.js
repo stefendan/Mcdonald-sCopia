918bcf6695de32853f5dd86baf7265bfbe9f6e9c
stefendan <138262255+stefendan@users.noreply.github.com>
2024-10-20 17:52:31 -0300

    Delete js/endereco_novo.js

--- js/endereco_novo.js
+++
@@ -1,9 +0,0 @@
-//lanche
-
-    // Recupera o endereço do localStorage
-    const enderecoSalvo = localStorage.getItem('endereco');
-    
-    // Atualiza o conteúdo do <p> com o endereço salvo
-    if (enderecoSalvo) {
-        document.querySelector('.endereco-info p').textContent = enderecoSalvo;
-    }
